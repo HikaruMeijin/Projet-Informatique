e85c12d19f7f0f718f983f0cc4d70aef755ef4bc
kyoshiro10500 <victorperson10@hotmail.fr>
2016-05-13 19:13:36 +0200

    Problème résolu (merci Nanashi)

--- tas.c
+++ tas.c
@@ -1,34 +1,41 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include "tas.h"
 
-#define Nbpersonnes 200
+#define TailleTableau 256
 
 tas creer_tas() {
-	tas tmp;
-	tmp->tableau = malloc(Nbpersonnes*sizeof(personne)); // Création du tableau de personnes
-	tmp->prochain_vide = 0; // Indice du dernier élément
-	return tmp;
+	tas t = (tas) malloc(sizeof(struct tas_base));
+	if(t == NULL) exit(EXIT_FAILURE);
+	
+	t->tableau = (personne*) malloc((TailleTableau+1)*sizeof(personne)); // Création du tableau de personnes
+	if(t->tableau == NULL) exit(EXIT_FAILURE);
+
+	int i;
+	for(i=0 ; i < TailleTableau+1 ; i++ ) t->tableau[i] = NULL ; // Initialisation des personnes 
+	t->prochain_vide = 0; // Indice du dernier élément
+	return t;
 }
 
 int tas_vide(tas t){
-	return (*t)->prochain_vide==0; //Vérifie qu'il n'y ait aucunes personnes dans le tas (dans le cas contraire, prochain_vide > 0)
+	return t->prochain_vide==0; //Vérifie qu'il n'y ait aucunes personnes dans le tas (dans le cas contraire, prochain_vide > 0)
 }
 
 void inserer_tas(tas* t, personne p){
 	int n = (*t)->prochain_vide;
 	(*t)->tableau[n]=p;
 	(*t)->prochain_vide=(*t)->prochain_vide+1; // Mise à jour pour le dernier élément
-	reorganiser_tas(t); // S'assure que la propriété du tas est conservée
+	reorganiser_tas_inserer(t); // S'assure que la propriété du tas est conservée
 }
 
 personne retirer_tas(tas* t){
 	int n = (*t)->prochain_vide;
 	personne prio = (*t)->tableau[0]; // Ces lignes inversent les personnes des indices n et 0
-	(*t)->tableau[0] = (*t)->tableau[n];
-	(*t)->tableau[n] = NULL;
+	(*t)->tableau[0] = (*t)->tableau[n-1];
+	(*t)->tableau[n-1] = NULL;
 	(*t)->prochain_vide=(*t)->prochain_vide-1; // Mise à jour pourr le dernier élément
-	reorganiser_tas(t); // S'assure que la propriété du tas est conservée
+	reorganiser_tas_retirer(t); // S'assure que la propriété du tas est conservée
 	return prio;
 }
 
@@ -36,45 +43,82 @@ void echanger_racine_fd(tas* t, int i){
 	personne tmp = (*t)->tableau[i]; // Echange dans le cas où on devrait inverser avec le fils droit
 	(*t)->tableau[i] = (*t)->tableau[2*i+2];
 	(*t)->tableau[2*i+2] = tmp;
-	i=2*i+2; // Remplace l'indice de travail avec son fils droit, se trouvant à l'indice 2*i+2
 }
 
 void echanger_racine_fg(tas* t, int i){
 	personne tmp = (*t)->tableau[i]; // Echange dans le cas où on devrait inverser avec le fils gauche
 	(*t)->tableau[i] = (*t)->tableau[2*i+1];
 	(*t)->tableau[2*i+1] = tmp;
-	i=2*i+1; // Remplace l'indice de travail avec son fils gauche, se trouvant à l'indice 2*i+1
 }
 
 int personne_priorite(tas* t, int ind){
 	return ((*t)->tableau[ind])->priorite;
 }
 
-void reorganiser_tas(tas* t){
+void reorganiser_tas_retirer(tas* t){
 	int i=0;
-	while(i < Nbpersonnes/2){ // S'assurer que l'on ne dépasse pas les 200 éléments 
-		if((*t)->tableau[2*i+1]==NULL){ //Si pas de fils gauche
-			if((*t)->tableau[2*i+2]==NULL) break; 
-			else{ // Si le fils droit existe
-				if (personne_priorite(t, i) < personne_priorite(t, 2*i+2)) echanger_racine_fd(t,i); // Si le fils droit a une plus grande priorité que la racine
-				else break; // Si la personne se trouvant à la racine a une plus grande priorité, on quitte la boucle
-			}
-		}
-		else if((*t)->tableau[2*i+2]==NULL){ // Si pas de fils droit
-			if((*t)->tableau[2*i+1]==NULL) break;
-			else{ // Si le fils gauche existe
-				if (personne_priorite(t, i) < personne_priorite(t, 2*i+1)) echanger_racine_fg(t,i); // Si le fils gauche a une plus grande priorité que la racine
-				else break; // Si la personne se trouvant à la racine a une plus grande priorité, on quitte la boucle
-			}
-		}
-		else {
-			if(personne_priorite(t, 2*i+1) >= personne_priorite(t, 2*i+2)){ 
-				if (personne_priorite(t, i) < personne_priorite(t, 2*i+1)) echanger_racine_fg(t,i); // Si le fils gauche a une plus grande priorité que la racine
-				else break; // Si la personne se trouvant à la racine a une plus grande priorité, on quitte la boucle
+	while(i < 100){ // S'assurer que l'on ne dépasse pas les 200 éléments 
+		if((*t)->tableau[i]!=NULL){
+			if((*t)->tableau[2*i+2]==NULL){ // Si pas de fils droit
+				if((*t)->tableau[2*i+1]==NULL) break;
+				else{ // Si le fils gauche existe
+					if (personne_priorite(t, i) < personne_priorite(t, 2*i+1)){
+						echanger_racine_fg(t,i); // Si le fils gauche a une plus grande priorité que la racine
+						i=2*i+1;
+					}
+					else break; // Si la personne se trouvant à la racine a une plus grande priorité, on quitte la boucle
+				}
 			}
 			else {
-				if (personne_priorite(t, i) < personne_priorite(t, 2*i+2)) echanger_racine_fd(t,i); // Si le fils droit a une plus grande priorité que la racine
-				else break; // Si la personne se trouvant à la racine a une plus grande priorité, on quitte la boucle
+				if(personne_priorite(t, 2*i+1) >= personne_priorite(t, 2*i+2)){ 
+					if (personne_priorite(t, i) < personne_priorite(t, 2*i+1)) {
+						echanger_racine_fg(t,i); // Si le fils gauche a une plus grande priorité que la racine
+						i=2*i+1;
+					}
+					else break; // Si la personne se trouvant à la racine a une plus grande priorité, on quitte la boucle
+				}
+				else {
+					if (personne_priorite(t, i) < personne_priorite(t, 2*i+2)) {
+						echanger_racine_fd(t,i); // Si le fils droit a une plus grande priorité que la racine
+						i=2*i+2;
+					}
+					else break; // Si la personne se trouvant à la racine a une plus grande priorité, on quitte la boucle
+				}
+			}
+		}
+		else break;
+	}
+}
+
+int est_pair(int ind){
+	if(ind % 2 == 0) return 1;
+	else return 0;
+}
+
+void reorganiser_tas_inserer(tas* t){
+	int i=(*t)->prochain_vide-1;
+	int j=0;
+	double k = 0.0;
+	while(i != 0){ // S'assurer que l'on atteigne bien l'élément 0
+		k = (double) i/2 ;
+		if(est_pair(i) == 0){ // Si l'indice considéré est impair (il n'y a donc pas de fils droit)
+			j = (int) floor(k); // Permet d'obtenir l'indice auquel se trouve la racine associée
+			if (personne_priorite(t, j) < personne_priorite(t, i)){
+				echanger_racine_fg(t,j); // Si le fils gauche a une plus grande priorité que la racine
+				i=j;
+			}
+			else break; // Si la personne se trouvant à la racine a une plus grande priorité, on quitte la boucle
+		}
+		else { // Si l'indice est pair (il y a alors deux fils)
+			if(personne_priorite(t, i-1) >= personne_priorite(t, i)) break; // Si le fils gauche a une priorité plus petite que le fils droit, aucuns changements à apporter
+			else {
+				j=(int) floor(k);
+				j--; // Permet d'obtenir l'indice auquel se trouve la racine associée
+				if (personne_priorite(t, j) < personne_priorite(t, i)) {
+					echanger_racine_fd(t,j); // Si le fils droit a une plus grande priorité que la racine
+					i=j;
+				}
+				else break; // Si la personne se trouvant à l'élément rajouté a une plus petite priorité, on quitte la boucle
 			}
 		}
 	}
--- tas.h
+++ tas.h
@@ -2,7 +2,6 @@
 #define TAS_H
 
 #include "listePersonne.h"
-#include "listePlanete.h"
 
 typedef struct tas_base {
   personne* tableau;
@@ -10,48 +9,58 @@ typedef struct tas_base {
 } * tas;
 
 /*@requires nothing
-  @ensigns nothing
+  @assigns nothing
   @ensures retourne un tas vide */
 tas creer_tas();
 
 /*@requires tas valide
-  @ensigns nothing
+  @assigns nothing
   @ensures retourne 1 si tas vide, O sinon */
 int tas_vide(tas);
 
 /*@requires pointeur sur tas valide et persone valide, non présente dans le tas
-  @ensigns tas
+  @assigns tas
   @ensures personne insérée à la bonne place dans le tas*/
 void inserer_tas(tas*, personne);
 
 /*@requires pointeur sur tas valide
-  @ensigns tas
-  ensures retirer personne en conservant nature de tas */
+  @assigns tas
+  @ensures retirer personne en conservant nature de tas */
 personne retirer_tas(tas*);
 
 /*@requires pointeur sur tas valide et indice valide
-  @ensigns nothing
-  ensures renvoi de la priorité de la personne concernée */
+  @assigns nothing
+  @ensures renvoi de la priorité de la personne concernée */
 int personne_priorite(tas* , int );
 
 /*@requires pointeur sur tas valide
-  @ensigns nothing
-  ensures échange la racine avec le fils droit */
-void echanger_racine_fd(tas*, int);
+  @assigns tas
+  @ensures échange la racine avec le fils droit */
+void echanger_racine_fd(tas*, int i);
 
 /*@requires pointeur sur tas valide
-  @ensigns nothing
-  ensures échange la racine avec le fils gauche */
-void echanger_racine_fg(tas*, int);
+  @assigns tas
+  @ensures échange la racine avec le fils gauche */
+void echanger_racine_fg(tas*, int i);
 
 /*@requires pointeur sur tas valide
-  @ensigns tas
-  ensures rééquilibrer le tas et conserver sa structure */
-void reorganiser_tas(tas*);
+  @assigns tas
+  @ensures rééquilibrer le tas après retrait d'un élément et conserver sa structure */
+void reorganiser_tas_retirer(tas*);
+
+/*@requires indice valide
+  @assigns nothing
+  @ensures la parité de l'indice ind (retourne 1 si impair, 0 sinon) */
+int est_pair(int ind);
 
 /*@requires pointeur sur tas valide
-  @ensigns tas
-  ensures libérer la zone mémoire associée au tas */
+  @assigns tas
+  @ensures rééquilibrer le tas après ajout d'un élément et conserver sa structure */
+void reorganiser_tas_inserer(tas* t);
+
+/*@requires pointeur sur tas valide
+  @assigns tas
+  @ensures libérer la zone mémoire associée au tas */
 void liberer_tas(tas*);
 
 #endif // TAS_H
