0466f0b205b00765b6302e76dc4d89b93553a4bd
HikaruMeijin <yann.lumia@gmail.com>
2016-05-13 15:27:37 +0200

    Update listePlanete.h

--- listePlanete.h
+++ listePlanete.h
@@ -8,7 +8,7 @@ typedef struct planete_base {
 
 typedef struct liste_planete_base {
 	planete val;
-	liste_planete next;
+	struct liste_planete_base * next;
 } * liste_planete;
 
 /*	@requires : nothing
@@ -26,7 +26,7 @@ liste_planete creer_liste_planete();
 	@ensures : retourne 1 si la liste est vide et 0 sinon */
 int liste_planete_vide(liste_planete);
 
-/*	@requires : un pointeur sur liste de planete valide et une planete valide, non présente dans la liste
+/*	@requires : un pointeur sur liste de planete valide et une planete valide, non prÃ©sente dans la liste
 	@assigns : la liste de planete
 	@ensures : la planete a ete inseree en tete de liste */
 void inserer_liste_planete(liste_planete*, planete);
