2283d7e4a79ac8a21b81701c5dc510151fe7accb
HikaruMeijin <yann.lumia@gmail.com>
2016-05-19 19:47:11 +0200

    Add files via upload

--- listePlanete.c
+++ listePlanete.c
@@ -7,10 +7,13 @@
 	@ensures : retourne une planete */
 planete creer_planete()
 {
-	planete pla = (planete) malloc(sizeof(struct planete_base)); /* on alloue dynamiquement la mÃ©moire nÃ©cessaire pour la planete 'pla' que l'on crÃ©e */
-	if (pla == NULL) { exit(EXIT_FAILURE); } /* si notre allocation mÃ©moire a Ã©chouÃ©, on arrÃªte l'exÃ©cution du programme */
+	planete pla = (planete) malloc(sizeof(struct planete_base)); /* on alloue dynamiquement la mémoire nécessaire pour la planete 'pla' que l'on crée */
+	if (pla == NULL) { exit(EXIT_FAILURE); } /* si notre allocation mémoire a échoué, on arrête l'exécution du programme */
 
-	return pla; /* l'allocation a rÃ©ussi, on retourne pla */
+	pla->nom = NULL;
+	pla->nbPlaces = 0;
+
+	return pla; /* l'allocation a réussi, on retourne pla */
 }
 
 /*	@requires : nothing
@@ -36,13 +39,13 @@ void inserer_liste_planete(liste_planete* listPla, planete pla)
 {
 	if (listPla == NULL) { exit(EXIT_FAILURE); }
 
-	liste_planete newLink = (liste_planete) malloc(sizeof(struct liste_planete_base)); /* allocation dynamique pour le nouveau maillon que l'on va insÃ©rer dans *listPla */
-	if (newLink == NULL) { exit(EXIT_FAILURE); } /* si notre allocation mÃ©moire a Ã©chouÃ©, on arrÃªte l'exÃ©cution du programme */
+	liste_planete newLink = (liste_planete) malloc(sizeof(struct liste_planete_base)); /* allocation dynamique pour le nouveau maillon que l'on va insérer dans *listPla */
+	if (newLink == NULL) { exit(EXIT_FAILURE); } /* si notre allocation mémoire a échoué, on arrête l'exécution du programme */
 
 	newLink->val = pla; /* le nouveau maillon prend la valeur pla */
 	newLink->next = *listPla;
 
-	*listPla = newLink; /* newLink devient la nouvelle tÃªte de notre liste */
+	*listPla = newLink; /* newLink devient la nouvelle tête de notre liste */
 }
 
 void affichePla(liste_planete listPla)
@@ -53,5 +56,29 @@ void affichePla(liste_planete listPla)
 		printf("[%s : %d places] -> ", curLink->val->nom, curLink->val->nbPlaces);
 		curLink = curLink->next;
 	}
-	printf("NULL\n");
+	printf("NULL");
+}
+
+/*	@requires : tableau de liste_planete tabListPla valide, vérifiant len(tabListPla) == tabSize
+	@assigns : nothing
+	@ensures : si namePla est le nom d'une planete contenue dans tabListPla,
+			   retourne le nombre de places restantes de cette planete;
+			   sinon, retourne -1 */
+int nb_places_planete(char* namePla, liste_planete* tabListPla, int tabSize)
+{
+	int i;
+	liste_planete curLink = NULL;
+
+	for (i = 0; i < tabSize; i++)
+	{
+		curLink = tabListPla[i];
+		while (curLink != NULL)
+		{
+			if (curLink->val->nom == namePla) { return curLink->val->nbPlaces; }
+
+			curLink = curLink->next;
+		}
+	}
+
+	return -1;
 }
--- listePlanete.h
+++ listePlanete.h
@@ -8,7 +8,7 @@ typedef struct planete_base {
 
 typedef struct liste_planete_base {
 	planete val;
-	struct liste_planete_base * next;
+	liste_planete next;
 } * liste_planete;
 
 /*	@requires : nothing
@@ -26,9 +26,16 @@ liste_planete creer_liste_planete();
 	@ensures : retourne 1 si la liste est vide et 0 sinon */
 int liste_planete_vide(liste_planete);
 
-/*	@requires : un pointeur sur liste de planete valide et une planete valide, non prÃ©sente dans la liste
+/*	@requires : un pointeur sur liste de planete valide et une planete valide, non présente dans la liste
 	@assigns : la liste de planete
 	@ensures : la planete a ete inseree en tete de liste */
 void inserer_liste_planete(liste_planete*, planete);
 
-# endif /* LISTPLA_H */
+/*	@requires : tableau de liste_planete tabListPla valide, vérifiant len(tabListPla) == tabSize
+	@assigns : nothing
+	@ensures : si namePla est le nom d'une planete contenue dans tabListPla,
+			   retourne le nombre de places restantes de cette planete;
+			   sinon, retourne -1 */
+int nb_places_planete(char* namePla, liste_planete* tabListPla, int tabSize);
+
+#endif /* LISTPLA_H */
